f9dabb83d12cdffd2d09cdfa1cb66cb4b6684b18
Shiyore <41596018+shiyore@users.noreply.github.com>
2021-03-28 17:25:36 -0700

    Fixed thread navigation

--- Pages/threads.php
+++ Pages/threads.php
@@ -34,7 +34,7 @@ require("../Classes/thread_data_service.php");
   <div class="media-body">
     <h5 class="mt-0 card-header"><?php echo $title; ?></h5>
     <p><?php echo $text; ?></p>
-    <a href="/app/Pages/edit_thread.php?id=<?php echo $thread->id;?>&title=<?php echo $thread->title;?>&text=<?php echo $thread->text;?>" class="stretched-link">Expand</a>
+    <a href="/Pages/edit_thread.php?id=<?php echo $thread->id;?>&title=<?php echo $thread->title;?>&text=<?php echo $thread->text;?>" class="stretched-link">Expand</a>
   </div>
 </div>
   
